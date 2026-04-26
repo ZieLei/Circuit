77e5dc14ed31c997d1f4761d09f9396314e86736
Ziel <kokoyokatasa@gmail.com>
2026-04-26 22:05:53 +0800

    Cart Page Added

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonLabel, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.page.html',
   styleUrls: ['./cart.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonLabel, IonImg, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class CartPage implements OnInit {
 
   constructor() { }
+
+  test_product = [
+        { 
+    name: 'Mechanical RGB Keyboard;askdkaskld asdkanlsd akjdsak dsha sdhaljs hdkaj shdkjah sdkajhsd', 
+    price: 1899, 
+    category: 'explore', 
+    row: 'Trending',
+    sales: 4500,
+    rating: 4.6,
+    reviews: 12700,
+    image: 'assets/images/rgb-kb.jpg',
+    count: 1
+  }
+  ]
+
+  formatPrice(price: number): string {
+    return '₱' + price.toLocaleString('en-PH');
+  }
 
   ngOnInit() {
   }
